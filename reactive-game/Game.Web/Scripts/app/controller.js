7c3a56f36455811c870ebd36182c2b3a7d3bc760
joseph ortiz <josepho0112345@gmail.com>
2016-01-16 21:07:10 -0500

    created Message Classes

---
+++ reactive-game/Game.Web/Scripts/app/controller.js
@@ -0,0 +1,7 @@
+﻿(function() {
+    var controllerModule = angular.module("controller-module", []);
+    controllerModule.controller('GameCtrl', function() {
+        var self = $scope;
+        self.hello = "world";
+    });
+}());
---
+++ reactive-game/Game.Web/Scripts/app/main.js
@@ -0,0 +1,3 @@
+﻿(function() {
+    var app = angular.module("game", ["controller-module"]);
+}());
